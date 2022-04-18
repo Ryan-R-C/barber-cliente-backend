fce50fc46f94619724bed03496e12d8304432777
Ryan-R-C <ryan.r.c.339ac@gmail.com>
2022-04-18 07:55:18 -0300

    added the routes to categoria

---
+++ src/api/categoria/index.ts
@@ -0,0 +1,30 @@
+export default (app) => {
+  app.post(
+    `/tenant/:tenantId/categoria`,
+    require('./categoriaCreate').default,
+  );
+  app.put(
+    `/tenant/:tenantId/categoria/:id`,
+    require('./categoriaUpdate').default,
+  );
+  app.post(
+    `/tenant/:tenantId/categoria/import`,
+    require('./categoriaImport').default,
+  );
+  app.delete(
+    `/tenant/:tenantId/categoria`,
+    require('./categoriaDestroy').default,
+  );
+  app.get(
+    `/tenant/:tenantId/categoria/autocomplete`,
+    require('./categoriaAutocomplete').default,
+  );
+  app.get(
+    `/categoria`,
+    require('./categoriaList').default,
+  );
+  app.get(
+    `/categoria/:id`,
+    require('./categoriaFind').default,
+  );
+};
