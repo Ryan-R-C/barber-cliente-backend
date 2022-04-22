fba455d7434dd5200f0916a481817744bb767126
Ryan-R-C <ryan.r.c.339ac@gmail.com>
2022-04-21 21:38:52 -0300

    added auth routes

---
+++ src/api/auth/authSignUp.ts
@@ -0,0 +1,21 @@
+import ApiResponseHandler from '../apiResponseHandler';
+import AuthService from '../../services/auth/authService';
+
+export default async (req, res, next) => {
+  try {
+    const payload = await AuthService.signup(
+      req.body.fullName,
+      req.body.email,
+      req.body.password,
+      req.body.phone,
+      req.body.role,
+      req.body.invitationToken,
+      req.body.tenantId,
+      req,
+    );
+
+    await ApiResponseHandler.success(req, res, payload);
+  } catch (error) {
+    await ApiResponseHandler.error(req, res, error);
+  }
+};
