82c97e4221312c956def719f1fcae58f05f2f8bf
Ryan-R-C <ryan.r.c.339ac@gmail.com>
2022-05-01 18:42:29 -0300

    finished with old services

---
+++ src/services/landingService.ts
@@ -0,0 +1,157 @@
+import Error400 from '../errors/Error400';
+import SequelizeRepository from '../database/repositories/sequelizeRepository';
+import { IServiceOptions } from './IServiceOptions';
+import LandingRepository from '../database/repositories/landingRepository';
+
+export default class LandingService {
+  options: IServiceOptions;
+
+  constructor(options) {
+    this.options = options;
+  }
+
+  async create(data) {
+    const transaction = await SequelizeRepository.createTransaction(
+      this.options.database,
+    );
+
+    try {
+    
+      const record = await LandingRepository.create(data, {
+        ...this.options,
+        transaction,
+      });
+
+      await SequelizeRepository.commitTransaction(
+        transaction,
+      );
+
+      return record;
+    } catch (error) {
+      await SequelizeRepository.rollbackTransaction(
+        transaction,
+      );
+
+      SequelizeRepository.handleUniqueFieldError(
+        error,
+        this.options.language,
+        'cerimonia',
+      );
+
+      throw error;
+    }
+  }
+
+  async update(id, data) {
+    const transaction = await SequelizeRepository.createTransaction(
+      this.options.database,
+    );
+
+    try {
+    
+      const record = await LandingRepository.update(
+        id,
+        data,
+        {
+          ...this.options,
+          transaction,
+        },
+      );
+
+      await SequelizeRepository.commitTransaction(
+        transaction,
+      );
+
+      return record;
+    } catch (error) {
+      await SequelizeRepository.rollbackTransaction(
+        transaction,
+      );
+
+      SequelizeRepository.handleUniqueFieldError(
+        error,
+        this.options.language,
+        'cerimonia',
+      );
+
+      throw error;
+    }
+  }
+
+  async destroyAll(ids) {
+    const transaction = await SequelizeRepository.createTransaction(
+      this.options.database,
+    );
+
+    try {
+      for (const id of ids) {
+        await LandingRepository.destroy(id, {
+          ...this.options,
+          transaction,
+        });
+      }
+
+      await SequelizeRepository.commitTransaction(
+        transaction,
+      );
+    } catch (error) {
+      await SequelizeRepository.rollbackTransaction(
+        transaction,
+      );
+      throw error;
+    }
+  }
+
+  async findById(id) {
+    return LandingRepository.findById(id, this.options);
+  }
+
+  async findAllAutocomplete(search, limit) {
+    return LandingRepository.findAllAutocomplete(
+      search,
+      limit,
+      this.options,
+    );
+  }
+
+  async findAndCountAll(args) {
+    return LandingRepository.findAndCountAll(
+      args,
+      this.options,
+    );
+  }
+
+  async import(data, importHash) {
+    if (!importHash) {
+      throw new Error400(
+        this.options.language,
+        'importer.errors.importHashRequired',
+      );
+    }
+
+    if (await this._isImportHashExistent(importHash)) {
+      throw new Error400(
+        this.options.language,
+        'importer.errors.importHashExistent',
+      );
+    }
+
+    const dataToCreate = {
+      ...data,
+      importHash,
+    };
+
+    return this.create(dataToCreate);
+  }
+
+  async _isImportHashExistent(importHash) {
+    const count = await LandingRepository.count(
+      {
+        importHash,
+      },
+      this.options,
+    );
+
+    return count > 0;
+  }
+}
